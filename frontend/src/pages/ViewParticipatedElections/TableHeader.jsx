fc68022431404bd7fbea893e28aac9bf64961c12
Mike5801 <A01610836@tec.mx>
2023-12-01 19:06:49 +0800

    refactor code

---
+++ frontend/src/pages/ViewParticipatedElections/TableHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TableHeader = ({ header }) => {
+  return <th className="bg-blue-500 text-white font-medium py-2">{header}</th>;
+};
+
+export default TableHeader;
