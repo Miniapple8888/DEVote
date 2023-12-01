db5a63d5a747be507bdb7cf08a1b6b18e529e7ed
Mike5801 <A01610836@tec.mx>
2023-12-01 11:43:25 +0800

    add comments to startElection

--- frontend/src/contracts/devote.js
+++ frontend/src/contracts/devote.js
@@ -3,10 +3,15 @@ import contractAbi from "./abi.json";
 import Web3 from "web3";
 
 // @ts-ignore
+// CONTRACT CONFIGURATION
 const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS;
 const web3 = new Web3(window.ethereum);
 const smartContract = new web3.eth.Contract(contractAbi, CONTRACT_ADDRESS);
 
+/**
+ * This method connects to the smart contract to start a new election
+ * @param candidates[] Array with the name of the candidates
+ */
 export const startElection = async (candidates) => {
   const accounts = await web3.eth.getAccounts();
   await smartContract.methods.createElection(candidates).send({
